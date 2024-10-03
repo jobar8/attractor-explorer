9291f99c2856f9a197c0a8bddfe2d13068152fe0
Joseph Barraud <joseph.barraud@gmail.com>
2024-10-03 09:18:09 +0100

    Add unit tests for attractors.py

---
+++ tests/test_attractors.py
@@ -0,0 +1,41 @@
+"""Test functions for the attractors.py module."""
+
+import pandas as pd
+import pytest
+
+import attractor_explorer.attractors as at
+
+
+def test_attractor():
+    """Test the Attractor class."""
+    attractor = at.Attractor()
+    assert attractor.a == 1.7
+    assert attractor.colormap == 'kgy'
+
+    fd = at.FractalDream()
+    assert fd.d == 1.2
+    assert fd.colormap == 'kgy'
+    assert fd.vals() == ['FractalDream', 'kgy', 0.0, 0.0, 1.7, 1.7, 0.6, 1.2]
+    assert fd.equations[0] == '$x_{n+1} = \\sin\\ by_n + c\\ \\sin\\ bx_n$'
+    assert fd.fn(1, 2, 3, 4, 5, 6) == pytest.approx((-2.7946542299162593, -1.535372981133688))
+
+    points = fd(n=5, x=1, y=2)
+    assert isinstance(points, pd.DataFrame)
+    assert points.shape == (5, 2)
+
+
+def test_attractor_compute():
+    """Test the compute() method of the Attractor class."""
+    fd = at.GumowskiMira()
+    all_points = fd.compute(n_points=10)
+    assert len(all_points) == 8
+    assert isinstance(all_points, pd.DataFrame)
+
+
+def test_parametersets():
+    """Test the ParameterSets class."""
+    params = at.ParameterSets(name='Attractors')
+    assert params.example == ['Svensson', 'fire', 0, 0, 1.4, 1.56, 1.4, -6.56]
+    assert len(params.examples) == 75
+    assert len(params.args('Svensson')) == 4
+    assert params.attractors['GumowskiMira'].__class__.__name__ == 'GumowskiMira'
